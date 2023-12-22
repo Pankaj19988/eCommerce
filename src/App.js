7d7a0b5948df3a7c9fc6056112f785f58808538d
Pankaj19988 <pankajmalankiya@gmail.com>
2023-12-22 21:10:07 +0530

    builded

--- src/App.js
+++ src/App.js
@@ -46,7 +46,6 @@ function App() {
           </Route>
           
           <Route exact path="/form" element={<Post />} />
-
           <Route exact path="/singup&login" element={<Singup_Login setProgress={setProgress}/>} />
 
         </Routes>
